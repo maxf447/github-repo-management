c015e825aa15c5d9ba75858c440093545b4da2dc
maxf447 <maxfrank@mtu.edu>
2026-02-12 17:19:14 -0500

    Added test counter program

---
+++ Counter.java
@@ -0,0 +1,8 @@
+class Test {
+    public static void main(String[] args) {
+      // Prints the numbers from 0 to 99
+      for (int i = 0; i < 100; i++) {
+        System.out.println(i);
+      }
+    }
+}
