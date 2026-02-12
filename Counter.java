4dbfaaa366bbbaf698f9d384c84fc8d6c972262d
maxf447 <maxfrank@mtu.edu>
2026-02-12 17:21:39 -0500

    Created dedicated function for counting loop

--- Counter.java
+++ Counter.java
@@ -1,8 +1,12 @@
 class Test {
     public static void main(String[] args) {
       // Prints the numbers from 0 to 99
-      for (int i = 0; i < 100; i++) {
-        System.out.println(i);
-      }
+      count(99);
+    }
+    // Helper function to count up to a number
+    public void count(int num) {
+        for (int i = 0; i < num + 1; i++) {
+            System.out.println(num);
+        }
     }
 }
